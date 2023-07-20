0e3772d294c07ac8f9c13d2f4268838d3a3475bc
Waqar Amin <vacaramin86@gmail.com>
2023-07-20 19:24:16 +0500

    Room initialzied

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/vacaramin/WebRTC-VideoChat-Go
 
 go 1.20
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+)
--- server/rooms.go
+++ server/rooms.go
@@ -1,1 +1,43 @@
 package server
+
+import (
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type Participant struct {
+	Host bool
+	Conn *websocket.Conn
+}
+
+type RoomMap struct {
+	Mutex sync.RWMutex
+	Map   map[string][]Participant
+}
+
+// Init initalizes a RoomMap struct
+func (r *RoomMap) Init() {
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+
+	r.Map = make(map[string][]Participant)
+}
+
+// Get Gets all the Participants in roomID
+func (r *RoomMap) Get(roomId string) []Participant {
+	r.Mutex.RLock()
+	defer r.Mutex.RUnlock()
+	return r.Map[roomId]
+}
+
+// CreateRoom Creates a New Room and returns the Room Id
+func (r *RoomMap) CreateRoom() string {
+	// generate a Unique ID
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+
+}
+
+func (r *RoomMap) DeleteRoom() {
+}
